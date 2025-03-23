b7142eb6e4799dcc36bd68622961a47fdc225f1f
Natanco1 <nata.oliveira2004@gmail.com>
2025-03-23 08:50:47 -0300

    feat: insert tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-blue-500 text-white text-2xl">
+      Tailwind is working! 🎨🚀
+    </div>
+  );
+}
+
+export default App;
